0be3c75acd15f4a9bf95ebccabb7555c4e077b83
VuTai96 <vutai13196@gmail.com>
2023-05-28 15:08:20 +0700

    JS basic, localStorage

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,27 @@ myImage.onclick = () => {
     myImage.setAttribute('src', 'images/firefox-icon.png');
   }
 };
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  const myName = prompt('Please enter your name!');
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  const storeName = localStorage.getItem('name');
+  myHeading.textContent = `Mozilla is cool, ${storeName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
